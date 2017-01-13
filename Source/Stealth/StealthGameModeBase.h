f75f26f0e7280230a52d9f72416ff5751919c07e
RagingGull <jopleh89@gmail.com>
2017-01-13 16:22:37 +0200

    Initial commit

---
+++ Source/Stealth/StealthGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "StealthGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEALTH_API AStealthGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
