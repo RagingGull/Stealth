0054151f133514407ffd9a5487840fefa7a2fac0
RagingGull <jopleh89@gmail.com>
2017-01-18 21:57:12 +0200

    Added 3rd person content pack

---
+++ Source/Stealth/Guards/BasicGuard.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Guard.h"
+#include "BasicGuard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEALTH_API ABasicGuard : public AGuard
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
